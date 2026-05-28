72e198a392b0f68bc8108bd5c789cc197b185946
V4nnura <52894271+V4nnura@users.noreply.github.com>
2026-05-28 08:59:26 -0300

    Revert name use on combatai.cc.

--- src/game/combatai.cc
+++ src/game/combatai.cc
@@ -601,15 +601,15 @@ Object* ai_danger_source(Object* critter)
     Object* targets[4];
     int index;
 
-    Object* whoHitMe = critter->data.critter.combat.whoHitMe;
-    if (whoHitMe == NULL || critter == whoHitMe) {
+    Object* who_hit_me = critter->data.critter.combat.whoHitMe;
+    if (who_hit_me == NULL || critter == who_hit_me) {
         targets[0] = NULL;
     } else {
-        if ((whoHitMe->data.critter.combat.results & DAM_DEAD) == 0) {
-            return whoHitMe;
+        if ((who_hit_me->data.critter.combat.results & DAM_DEAD) == 0) {
+            return who_hit_me;
         } else {
             if (whoHitMe->data.critter.combat.team != critter->data.critter.combat.team) {
-                targets[0] = ai_find_nearest_team(critter, whoHitMe, 1);
+                targets[0] = ai_find_nearest_team(critter, who_hit_me, 1);
             } else {
                 targets[0] = NULL;
             }
